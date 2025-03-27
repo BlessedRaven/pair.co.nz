a3d7dac8859ac71bf34fb64a6120de6e67deee14
BlessedRaven <jakeryanlankshear@gmail.com>
2025-03-27 17:14:02 +1300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -87,3 +87,27 @@ function animate() {
 }
 
 animate();
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static('public')); // Serve your static files
+
+io.on('connection', (socket) => {
+    console.log('New deity connected');
+    
+    socket.on('message', (data) => {
+        io.emit('message', data);
+    });
+    
+    socket.on('disconnect', () => {
+        console.log('Deity departed');
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Pantheon Chat running on port 3000');
+});
